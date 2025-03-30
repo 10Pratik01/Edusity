2ad5837eeb824d99a6881ebe65f283b42dd8340d
10Pratik01 <using0this@gmail.com>
2025-03-30 14:33:52 +0530

    Content.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,9 +6,9 @@ const Content = () => {
       <div className=" bg-[url(/src/assets/hero.png)] h-screen w-screen bg-cover bg-center">
         <div className="bg-gradient-to-r from-indigo-900/60 to-indigo-900/60 b h-screen w-screen display flex justify-center items-center">
             <div className="text-block text-white text-center w-200">
-                <h1 className="text-5xl text-cente">We Ensure better education for a better world</h1>
-                <p className="line-clamp-3 m-4">Our cutting-edge curriculum is designed to empover students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
-                <button className="bg-white text-black rounded-full p-2 pt-1 pb-1 ">Explore more</button>
+                <h1 className="text-6xl text-cente">We Ensure better education for a better world</h1>
+                <p className="line-clamp-3 m-4 text-1xl">Our cutting-edge curriculum is designed to empover students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
+                <button className="bg-white text-black rounded-full p-3 pt-2 pb-2 text-l cursor-pointer hover:bg-grey-200 ">Explore more</button>
             </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import Content from './Content.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/>
     <Content/>
   </StrictMode>,
 )

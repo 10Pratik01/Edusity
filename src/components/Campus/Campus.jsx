acddf789a43acb92a91e3e6fe838a4edffc0d5c5
10Pratik01 <using0this@gmail.com>
2025-04-07 16:57:40 +0530

    Completed campus

--- src/components/Campus/Campus.jsx
+++ src/components/Campus/Campus.jsx
@@ -12,13 +12,13 @@ const Campus = () => {
         className="flex justify-center gap-6 mt-10 mb-10"
         id="campus_images "
       >
-        <img className="w-55 h-70 rounded-2xl" src={galery1} alt="" />
-        <img className="w-55 h-70 rounded-2xl" src={galery2} alt="" />
-        <img className="w-55 h-70 rounded-2xl" src={galery3} alt="" />
-        <img className="w-55 h-70 rounded-2xl" src={galery4} alt="" />
+        <img className="w-55 h-70 rounded-2xl cursor-pointer duration-75 hover:scale-105 " src={galery1} alt="" />
+        <img className="w-55 h-70 rounded-2xl cursor-pointer duration-75 hover:scale-105" src={galery2} alt="" />
+        <img className="w-55 h-70 rounded-2xl cursor-pointer duration-75 hover:scale-105" src={galery3} alt="" />
+        <img className="w-55 h-70 rounded-2xl cursor-pointer duration-75 hover:scale-105" src={galery4} alt="" />
       </div>
       <div className="flex justify-center mb-10 align-center items-center mt-1/2">
-        <button className="bg-blue-700 text-white rounded-full p-4 pt-3 pb-3 flex">See more images <img className="w-5 h-3 ml-3" src={whitearr} alt="" /></button>
+        <button className="bg-blue-700 text-white rounded-full p-4 pt-3 pb-3 flex cursor-pointer hover:bg-blue-600">See more images <div className="flex items-center"><img className="w-5 h-3 ml-" src={whitearr} alt="" /></div></button>
       </div>
     </div>
   );
